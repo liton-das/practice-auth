b142d881c32d3332d59a7790f6dc9b0089e62b7d
liton-das <litonchandra094@gmail.com>
2025-11-05 07:44:12 +0300

    nav design completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,66 +2,57 @@ import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router'
 import { GoGraph } from "react-icons/go";
 import { IoIosArrowForward } from "react-icons/io";
+import {
+  AppstoreOutlined,
+  CalendarOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+import { Divider, Menu, Switch } from 'antd';
+const items = [
+  {
+    key: 'sub1',
+    label: <p className='text-base'>Products</p>,
+    icon: <AppstoreOutlined />,
+    children: [
+      { 
+        key: '1', 
+        label: <NavLink to={'#'} className={'text-[14px] '}>All Products</NavLink> 
+      },
+      { 
+        key: '2', 
+        label: <NavLink to={'#'} className={'text-[14px] '}>All Category</NavLink>
+      },
+      { 
+        key: '3', 
+        label: <NavLink to={'#'} className={'text-[14px] '}>Add Product</NavLink>
+      },
+      { 
+        key: '4', 
+        label: <NavLink to={'#'} className={'text-[14px] '}>Add Category</NavLink>
+      },
+    ],
+  }
+];
 const Navbar = () => {
-    const [accordian,setAccordian]=useState(false)
-    // const handleAccordian function
-    const handleAccordian=()=>{
-        setAccordian(!accordian)
-    }
+    const [mode, setMode] = useState('inline');
+  const [theme, setTheme] = useState('light');
+  const changeMode = value => {
+    setMode(value ? 'vertical' : 'inline');
+  };
+  const changeTheme = value => {
+    setTheme(value ? 'dark' : 'light');
+  };
   return (
     <>
       <nav className="w-[252px] h-screen shadow-2xl bg-white">
-        <NavLink to={"/"} className={"pt-12 pl-5 pr-[107px] flex items-center gap-4 text-[20px]"}>
-          <GoGraph />
-          DashBoard
-        </NavLink>
-        <p className="mt-[51px]  pl-10 text-[14px] mb-[34px] font-inter">Admin Panel</p>
-        <div className="container px-6 mb-[23px] cursor-pointer ">
-          <div className="px-6 mb-[23px]">
-         
-              <div onClick={handleAccordian} className="flex items-center justify-between">
-                <p>Products </p>
-                <IoIosArrowForward className={`mr-[51px] ${accordian ?'transform-[rotate(90deg)]':'mr-[51px] transform-[rotate(-360deg)]'}`} />
-              </div>
-            {
-                accordian && 
-                <>
-                <Link to={"#"} className="text-sky-600 text-[13px] mt-2 block font-semibold">
-                  All Product
-                </Link>
-                <Link to={"#"} className="text-sky-600 text-[13px] mt-2 block font-semibold">
-                  All Category
-                </Link>
-                <Link to={"#"} className="text-sky-600 text-[13px] mt-2 block font-semibold">
-                  Add Product
-                </Link>
-                <Link to={"#"} className="text-sky-600 text-[13px] mt-2 block font-semibold">
-                  Add Category
-                </Link>
-              </>
-            }
-              
-        
-          </div>
-          <div className="px-6 mb-[23px]">
-            <div className="flex items-center justify-between">
-              <p>Orders </p>
-              <IoIosArrowForward className="mr-[51px]" />
-            </div>
-            {/* <Link to={'#'} className='text-[14px] block font-semibold'>
-                        All Product
-                    </Link> */}
-          </div>
-          <div>
-            <div className="flex px-6 mb-[23px] items-center justify-between">
-              <p>Clients </p>
-              <IoIosArrowForward className="mr-[51px]" />
-            </div>
-            {/* <Link to={'#'} className='text-[14px] block font-semibold'>
-                        All Product
-                    </Link> */}
-          </div>
-        </div>
+        <NavLink to={"/"} className={"pt-12 pl-5 pr-[107px] flex items-center gap-4 text-[20px]"}><GoGraph />DashBoard</NavLink>
+        <p className="mt-[51px] border-b border-slate-200 pb-2  pl-10 text-[13px] font-inter">Admin Panel</p>
+        {/* <Switch onChange={changeMode} /> Change Mode */}
+      <Divider type="vertical" />
+      <Menu defaultSelectedKeys={['1']} mode={mode} theme={theme} items={items} />
+      <NavLink to={"/"} className={"pl-6  flex items-center gap-2 text-[16px] text-sky-600 mb-2 font-inter "}>Orders</NavLink>
+      <NavLink to={"/"} className={"pl-6  flex items-center gap-2 text-[16px] text-sky-600 font-inter "}>Client List</NavLink>
       </nav>
     </>
   );
