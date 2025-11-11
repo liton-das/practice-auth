75bdde9cc5d66661e174b4780129067e7ac8c4cf
liton-das <litonchandra094@gmail.com>
2025-11-11 22:48:57 +0300

    category model updated

---
+++ server/src/model/category.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+const categorySchema = new Schema({
+    categoryName:{
+        type:String,
+        required:true
+    },
+    categoryImage:{
+        type:String,
+        required:true
+    },
+    createdBy:{
+        type:Schema.Types.ObjectId,
+        ref:'auths'
+    },
+    adminApproval:{
+        type:String,
+        enum:['approved','pendding','cancel'],
+        default:'pendding'
+    }
+},{timestamps:true})
+module.exports = model('category',categorySchema)
