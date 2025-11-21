e41c14e8e148a3c78b3568d5770f3ed5a2326574
liton-das <litonchandra094@gmail.com>
2025-11-21 23:52:47 +0600

    Product schema design completed

--- server/src/model/products.js
+++ server/src/model/products.js
@@ -2,18 +2,84 @@ const { Schema, model } = require("mongoose");
 
 const productSchema = new Schema(
   {
-    tags: [
+    title: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    subImages: [
       {
-        tagName: {
+        type: String,
+        default: null,
+      },
+    ],
+    price: {
+      type: String,
+      required: true,
+    },
+    varient: [
+      {
+        varientName: {
           type: String,
-          required: true,
+          default: null,
         },
-        tagValue: {
-          type: String,
-          required: true,
+        extraPrice: {
+          type: Number,
+          default: null,
         },
       },
     ],
+    categoryId: {
+      type: Schema.Types.ObjectId,
+      ref: "categories",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    review: [
+      {
+        reviewId: {
+          type: Schema.Types.ObjectId,
+          ref: "auths",
+        },
+        review:{
+          type:String,
+          default:null
+        }
+      },
+    ],
+    discountPrice:{
+      type:String,
+      default:null
+    },
+    tags:[
+      {
+        type:String,
+        default:null
+      }
+    ],
+    stock:{
+      type:Number,
+      required:true
+    },
+    sku:{
+      type:String,
+      required:true
+    },
+    adminApproval:{
+        type:String,
+        enum:['pending','approved','cancle'],
+        default:'pending'
+    },
+    slug:{
+      type:String,
+      required:true
+    }
   },
   { timestamps: true }
 );
