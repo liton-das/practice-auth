f6b0d73525cdbf3d01337c4bfb83e6f531c0a6c7
liton-das <litonchandra094@gmail.com>
2025-11-22 23:51:57 +0600

    work with product controller

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -7,7 +7,12 @@ const addProductController = async(req,res)=>{
         const sku = skuGenerator(title)
         const slug = slugGenerator(title)
         // upload images
-        console.log(req.files)
+        console.log(req.files,'in req')
+        const images = req.files.subImages?.map(item=>{
+            console.log(`item in map ${item.path}`)
+            return item
+        })
+        console.log(images.path)
     } catch (error) {
         return res.status(500).json({message:'Internal server error',error})
     }
