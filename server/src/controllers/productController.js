8e728ada54460987d38823312f941fb948e2fffa
liton-das <litonchandra094@gmail.com>
2025-11-26 23:11:52 +0600

    create product controller completed

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -11,29 +11,31 @@ const addProductController = async(req,res)=>{
         // upload thumbnail image
         const thumbnail = req.files.thumbnail[0].path
         const thumbImg = await uploadCloudinary(thumbnail)
-        console.log('iam thumbnail',thumbImg)
         const images =req.files.subImages?.map((item)=>{
             return item.path
         })
         // upload subImages 
-        const subImgs= Promise.all( images?.map(async(item)=>{
+        const subImgs=await Promise.all(images?.map(async(item)=>{
             const subImages =await uploadCloudinary(item)
             return subImages
         }))
-        console.log('iam subImages',subImgs)
-        return res.send('success')
         // all data save to the database
-        // new products({
-        //     title,
-        //     price,
-        //     varient,
-        //     categoryId,
-        //     description,
-        //     review,
-        //     discountPrice,
-        //     tags,
-        //     stock
-        // })
+       await new products({
+            title,
+            thumbnail:thumbImg,
+            subImages:subImgs,
+            price,
+            varient:JSON.parse(varient),
+            categoryId,
+            description,
+            review:JSON.parse(review),
+            discountPrice,
+            tags,
+            stock,
+            sku,
+            slug
+        }).save()
+    return res.status(201).json({message:'Product added successfully'})
     } catch (error) {
         console.log(error)
         return res.status(500).json({message:'Internal server error',error})
