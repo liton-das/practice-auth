7d58f88cd562269dab162b208e3900ad88278d29
liton-das <litonchandra094@gmail.com>
2025-12-08 20:24:11 +0600

    updated cart controller

--- server/src/controllers/addToCart.js
+++ server/src/controllers/addToCart.js
@@ -4,17 +4,16 @@ const products = require("../model/products")
 // add to cart 
 const addToCartProdut=async(req,res)=>{
     try {
-        const {creatorId,productId,varients}=req.body
-        const existProduct = await cart.findOne({productId,creatorId})
-        const [varient]=varients
-        // return console.log(varient.varient)
-        if(existProduct != null && existProduct != ''){
-            if(existProduct.varients.find((item)=>item.varient == varient.varient)){
-           existProduct.qty += 1
+        const {creatorId,productId,varients}=req.body // get cart information 
+        const existProduct = await cart.findOne({productId,creatorId}) // find exist product from db
+        const [varient]=varients // distructure 
+        if(existProduct != null && existProduct != ''){ // check validation no product in db then added a new cart
+            if(existProduct.varients.find((item)=>item.varient == varient.varient)){ // if same variant have in db then just increase the quantity
+            existProduct.qty += 1
            await existProduct.save()
            return res.status(200).json({message:'Product quantity updated'})
         }
-        if(existProduct.varients.find((item)=>item.varient !== varient.varient)){
+        if(existProduct.varients.find((item)=>item.varient !== varient.varient)){ // if same product but not same variant the added new variant 
             existProduct.varients.push(varient)
             existProduct.qty += 1
             await existProduct.save()
@@ -25,7 +24,7 @@ const addToCartProdut=async(req,res)=>{
                 creatorId,
                 productId,
                 varients
-            }).save()
+            }).save() // new cart added in db
         return res.status(201).json({message:'Cart Added'})
     } catch (error) {
         console.log(error)
